3739dbdea97b08907efd0bde7436d6af2aa5ef12
Giovanni-dev-byte <75442055+Giovanni-dev-byte@users.noreply.github.com>
2020-12-03 15:19:04 -0300

    Add files via upload

---
+++ exercicio29.java
@@ -0,0 +1,24 @@
+
+public class exercicio29 {
+public static void main(String[] args) {
+import java.util
+	
+	java.util.Scanner Entrada = new Scanner (System.in);
+
+	double x = 1 <= 100;
+	
+	System.out.println("Entre com o número:");
+	
+	x = Entrada.nextDouble();
+	
+	for (double x=1 ; x<=100 ; x = x + 10){
+		
+		System.out.println(x);
+		
+	for (double x=1 ; x<=100 ; i = x + 10){
+	
+		System.out.println(x+"Multiplo de 10");
+
+	}
+
+}
---
+++ exercicio31.java
@@ -0,0 +1,34 @@
+public class exercicio31 {
+public static void main(String[] args) {
+import java.util
+
+java.util.Scanner Entrada = new Scanner (System.in);
+
+	double x = 9;
+	
+	System.out.println("Entre com o valor");
+	x = Entrada.nextDouble();
+	
+	for (x==9)
+		
+		System.out.println("O valor do primeiro fatorial é 1");
+
+			System.out.println("O valor do segundo fatorial é 2");
+
+			System.out.println("O valor do terceiro fatorial é 6");
+
+			System.out.println("O valor do quarto fatorial é 24");
+
+			System.out.println("O valor do quinto fatorial é 120");
+
+			System.out.println("O valor  sexto fatorial é 720");
+
+			System.out.println("O valor sétimo fatorial é 5040");
+
+			System.out.println("O valor oitavo fatorial é 40320");
+
+			System.out.println("O valor nono fatorial é 362880");
+
+	}
+
+
